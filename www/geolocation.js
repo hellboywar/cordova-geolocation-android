8d70f771419d95f0616dc8a3483de2081e0d788c
Andrey Sukhonosov <sukhonosov@flexites.org>
2016-11-10 15:18:34 +0500

    upd

--- www/geolocation.js
+++ www/geolocation.js
@@ -1,5 +1,7 @@
-window.echo = function(str, callback) {
-  cordova.exec(callback, function(err) {
-    callback('Nothing to echo.');
-  }, "Echo", "echo", [str]);
+/*global cordova, module*/
+
+module.exports = {
+  echo: function (name, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "Echo", "echo", [name]);
+  }
 };
